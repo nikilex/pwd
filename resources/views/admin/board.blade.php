5efa79dc52cbbe70ab22f6682f8fd36db508e13c
Алексей Никитин <a.nikitin@k-telecom.org>
2024-04-09 18:51:38 +0500

    fix

--- resources/views/admin/board.blade.php
+++ resources/views/admin/board.blade.php
@@ -1,10 +1,14 @@
 @extends(backpack_view('blank'))
 
 @section('content')
+
 <div class="jumbotron" id="app">
+@if($board)
     <h1 class="mb-4">{{ $board->title }}</h1>
 
     <Board board-id="{{ $board->id }}" />
-
+@else
+    <h5>Досок пока нет</h5>
+@endif
 </div>
 @endsection
